23be193d346041042e4fb536e3fa4672e55cbb63
Izzy <extremerightwing@gmail.com>
2013-12-09 22:42:28 -0800

    increment

--- init.lua
+++ init.lua
@@ -1,4 +1,23 @@
 local modpath = minetest.get_modpath("morecrops")
 
+morecrops = {}
+
 dofile(modpath.."/croplist.lua")
 dofile(modpath.."/annuals.lua")
+
+
+for i in ipairs(morecrops.crops) do
+	
+	local type = morecrops.crops[i].type
+	
+	
+	if type == "annual" then
+		morecrops.register_annual(morecrops.crops[i])
+	end
+	
+	
+	
+	
+	
+end
+
